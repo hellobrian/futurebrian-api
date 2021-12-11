b09afcf173a10363e9a9d12dd2307a4e510091f1
hellobrian <brianhan@hey.com>
2021-12-10 20:09:30 -0600

    delete more stufffffff

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,29 +10,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          futurebrian-api
-        </h1>
+      <main>
+        <h1>futurebrian-api</h1>
 
         <ul>
-          <li><a href="/api/keyboards">GET keyboards</a></li>
-          <li><a href="/api/keycaps">GET keycaps</a></li>
+          <li>
+            <a href="/api/keyboards">GET keyboards</a>
+          </li>
+          <li>
+            <a href="/api/keycaps">GET keycaps</a>
+          </li>
         </ul>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
+          Powered by{" "}
+          <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
