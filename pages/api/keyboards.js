922bd5087eeba5524c9afd1948c0554ce51f07fd
hellobrian <brianhan@hey.com>
2021-12-10 19:28:09 -0600

    initial commit

---
+++ pages/api/keyboards.js
@@ -0,0 +1,20 @@
+import prisma from '../../lib/prisma'
+
+export default async function assetHandler(req, res) {
+    const {method }= req;
+ switch (method) {
+     case "GET":
+         try {
+                const keyboards = await prisma.keyboard.findMany()
+                res.status(200).json(keyboards)
+         } catch (e) {
+             console.error("Request error", e)
+             res.status(500).json({ error: 'Error fetching keyboards'})
+         }
+         break;
+     default:
+         res.setHeader("Allow", ["GET"])
+         res.status(405).end(`Method ${method} Not Allowed`)
+         break;
+ }   
+}
